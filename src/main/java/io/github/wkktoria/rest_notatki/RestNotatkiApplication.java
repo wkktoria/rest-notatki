bb49b8b33fdbb525bf5aad98acdbf2064c83ebca
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-12-12 11:37:22 +0100

    Init project

---
+++ src/main/java/io/github/wkktoria/rest_notatki/RestNotatkiApplication.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.rest_notatki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestNotatkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestNotatkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wkktoria/rest_notatki/RestNotatkiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.rest_notatki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestNotatkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
