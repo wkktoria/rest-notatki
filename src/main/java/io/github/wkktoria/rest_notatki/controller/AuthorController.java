5df8db8ea73a126f5c0bb4c413f01a05e0f3f06f
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2025-12-12 14:41:28 +0100

    Document API endpoints

--- src/main/java/io/github/wkktoria/rest_notatki/controller/AuthorController.java
+++ src/main/java/io/github/wkktoria/rest_notatki/controller/AuthorController.java
@@ -3,6 +3,12 @@ package io.github.wkktoria.rest_notatki.controller;
 import io.github.wkktoria.rest_notatki.dto.author.AuthorDto;
 import io.github.wkktoria.rest_notatki.dto.author.CreateAuthorDto;
 import io.github.wkktoria.rest_notatki.service.AuthorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,16 +24,55 @@ public class AuthorController {
     private AuthorService authorService;
 
     @GetMapping(value = "/authors", version = "1.0")
+    @Operation(summary = "Gets list of authors")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok",
+                    content = @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = AuthorDto.class)))
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "text/plain")
+            )
+    })
     public ResponseEntity<List<AuthorDto>> getAuthors() {
         return ResponseEntity.ok(authorService.getAllAuthors());
     }
 
     @GetMapping(value = "/authors/{id}", version = "1.0")
+    @Operation(summary = "Gets author by ID", description = "Author must exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthorDto.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "Author not found",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "text/plain")
+            )
+    })
     public ResponseEntity<AuthorDto> getAuthor(@PathVariable final Long id) {
         return ResponseEntity.ok(authorService.getAuthorById(id));
     }
 
     @PostMapping(value = "/authors", version = "1.0")
+    @Operation(summary = "Creates new author")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthorDto.class))
+            ),
+            @ApiResponse(responseCode = "400", description = "Invalid input supplied",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @ApiResponse(responseCode = "409", description = "Author with the same name already exists",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "text/plain")
+            )
+    })
     public ResponseEntity<AuthorDto> createAuthor(@RequestBody @Valid final CreateAuthorDto createAuthorDto) {
         return new ResponseEntity<>(authorService.createAuthor(createAuthorDto), HttpStatus.CREATED);
     }
--- src/main/java/io/github/wkktoria/rest_notatki/controller/NoteController.java
+++ src/main/java/io/github/wkktoria/rest_notatki/controller/NoteController.java
@@ -1,8 +1,15 @@
 package io.github.wkktoria.rest_notatki.controller;
 
+import io.github.wkktoria.rest_notatki.dto.author.AuthorDto;
 import io.github.wkktoria.rest_notatki.dto.note.CreateNoteDto;
 import io.github.wkktoria.rest_notatki.dto.note.NoteDto;
 import io.github.wkktoria.rest_notatki.service.NoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,21 +25,72 @@ public class NoteController {
     private final NoteService noteService;
 
     @GetMapping(value = "/notes", version = "1.0")
+    @Operation(summary = "Gets list of notes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok",
+                    content = @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = NoteDto.class)))
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "text/plain")
+            )
+    })
     public ResponseEntity<List<NoteDto>> getNotes() {
         return ResponseEntity.ok(noteService.getAllNotes());
     }
 
     @GetMapping(value = "/notes/{id}", version = "1.0")
+    @Operation(summary = "Gets note by ID", description = "Note must exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = NoteDto.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "Note not found",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "text/plain")
+            )
+    })
     public ResponseEntity<NoteDto> getNote(@PathVariable final Long id) {
         return ResponseEntity.ok(noteService.getNoteById(id));
     }
 
     @PostMapping(value = "/notes", version = "1.0")
+    @Operation(summary = "Creates new note")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthorDto.class))
+            ),
+            @ApiResponse(responseCode = "400", description = "Invalid input supplied",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @ApiResponse(responseCode = "404", description = "Author with supplied ID not found",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "text/plain")
+            )
+    })
     public ResponseEntity<NoteDto> createNote(@RequestBody @Valid final CreateNoteDto createNoteDto) {
         return new ResponseEntity<>(noteService.createNote(createNoteDto), HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/notes/{id}", version = "1.0")
+    @Operation(summary = "Deletes note by ID", description = "Note must exist")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No content",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "404", description = "Note not found",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "text/plain")
+            )
+    })
     public ResponseEntity<Void> deleteNote(@PathVariable final Long id) {
         noteService.deleteNoteById(id);
         return ResponseEntity.noContent().build();
